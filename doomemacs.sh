b9536100c7689016dce3b0f06b09620583cc3653
Mark Koberlein <mkoberlein@gmail.com>
2023-06-23 14:45:02 -0400

    updated doomemacs to download 28.2 snapshot instead of git master

--- doomemacs.sh
+++ doomemacs.sh
@@ -3,17 +3,19 @@
 sudo apt install -y build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo autoconf ninja-build gettext cmake unzip curl fd-find ripgrep
 
 cd ~/
-git clone git://git.sv.gnu.org/emacs.git
-
-cd emacs
+wget https://git.savannah.gnu.org/cgit/emacs.git/snapshot/emacs-28.2.tar.gz
+tar -xf emacs-28.2.tar.gz
+cd emacs-28.2
 ./autogen.sh
 ./configure --with-pgtk
 make -j8
 sudo make install
 
 cd ~/
-mv emacs trash
+mv emacs-28.2 trash
+mv emacs-28.2.tar.gz trash
 
 cd ~/
+rm -rf ~/.config/emacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 
 sudo apt update
-sudo apt install -y ansible libfuse2 git git-lfs gh vim tmux gnome-tweaks build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo autoconf ninja-build gettext cmake unzip curl fd-find ripgrep ninja-build gettext cmake unzip curl python3-neovim
+sudo apt install -y epiphany-browser ansible libfuse2 git git-lfs gh vim tmux gnome-tweaks build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo autoconf ninja-build gettext cmake unzip curl fd-find ripgrep ninja-build gettext cmake unzip curl python3-neovim
 
 mkdir ~/repos
 mkdir ~/trash
