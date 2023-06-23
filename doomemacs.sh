edd69739bc6a6efdff263a33fa61e7990979f5a1
Mark Koberlein <mkoberlein@gmail.com>
2023-06-23 15:50:50 -0400

    updated emacs install to 29

--- doomemacs.sh
+++ doomemacs.sh
@@ -3,9 +3,9 @@
 sudo apt install -y build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo autoconf ninja-build gettext cmake unzip curl fd-find ripgrep
 
 cd ~/
-wget https://git.savannah.gnu.org/cgit/emacs.git/snapshot/emacs-28.2.tar.gz
-tar -xf emacs-28.2.tar.gz
-cd emacs-28.2
+wget https://git.savannah.gnu.org/cgit/emacs.git/snapshot/emacs-29.0.92.tar.gz
+tar -xf emacs-29.0.92.tar.gz
+cd emacs-29.0.92
 ./autogen.sh
 ./configure --with-pgtk
 make -j8
