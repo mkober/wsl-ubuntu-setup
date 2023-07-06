815e79758ce04ed5b442a5aaef8268e1b14455f7
Mark Koberlein <mkoberlein@gmail.com>
2023-07-06 16:02:57 -0400

    added git editor config

--- git.sh
+++ git.sh
@@ -5,3 +5,4 @@ sudo apt install git gh git-lfs
 git config --global user.name "Mark Koberlein"
 git config --global user.email "mkoberlein@gmail.com"
 git config --global init.defaultBranch main
+git config --global core.editor "nvim"
